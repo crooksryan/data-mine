559f0e508162498b0bd0723a8140449c07b32290
crooksryan <rdc5410@psu.edu>
2023-11-30 13:43:25 -0500

    added monitor to batch file and fixed merge conflict

--- startup.bat
+++ startup.bat
@@ -4,6 +4,6 @@ echo Running Mass_Gather
 python mass_gather.py
 
 echo Running Machine
-python scheduler.py
+python scheduler.py && python monitor.py
 
 echo Finished
--- static/plotting.js
+++ static/plotting.js
@@ -42,123 +42,100 @@ async function dataGraphing() {
 
 
 
-// const aapl = document.getElementById('aapl');
+ const aapl = document.getElementById('aapl');
 
-<<<<<<< HEAD
-new Chart(voo, {
-  type: 'line',
-  data: {
-    labels: xVals,
-    datasets: [{
-      label: 'Price',
-      data: yVals,
-      backgroundColor: 'rgba(115, 0, 115, 1.0)',
-      borderColor: "rgba(95, 0, 95, 1.0)",
-      tension: 0.15
-    }]
-  },
-  options: {
-    plugins:{
-      legend:{
-        display: false
-      }
-    }
-  }
+ let xVals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ let yVals = [10, 18, 13, 16, 12, 13, 22, 15, 16, 18]
+
+ new Chart(aapl, {
+ 	type: 'line',
+ 	data: {
+ 		labels: xVals,
+ 		datasets: [{
+ 			label: 'Price',
+ 			data: yVals,
+ 			backgroundColor: 'rgba(115, 0, 115, 1.0)',
+ 			borderColor: "rgba(95, 0, 95, 1.0)",
+ 			tension: 0.15
+ 		}]
+ 	},
+ 	options: {
+ 		plugins: {
+ 			legend: {
+ 				display: false
+ 			}
+ 		}
+ 	}
 });
-=======
-// let xVals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// let yVals = [10, 18, 13, 16, 12, 13, 22, 15, 16, 18]
 
-// new Chart(aapl, {
-// 	type: 'line',
-// 	data: {
-// 		labels: xVals,
-// 		datasets: [{
-// 			label: 'Price',
-// 			data: yVals,
-// 			backgroundColor: 'rgba(115, 0, 115, 1.0)',
-// 			borderColor: "rgba(95, 0, 95, 1.0)",
-// 			tension: 0.15
-// 		}]
-// 	},
-// 	options: {
-// 		plugins: {
-// 			legend: {
-// 				display: false
-// 			}
-// 		}
-// 	}
-// });
+ const amzn = document.getElementById('amzn');
 
-// const amzn = document.getElementById('amzn');
-
-// new Chart(amzn, {
-// 	type: 'line',
-// 	data: {
-// 		labels: xVals,
-// 		datasets: [{
-// 			label: 'Price',
-// 			data: yVals,
-// 			backgroundColor: 'rgba(115, 0, 115, 1.0)',
-// 			borderColor: "rgba(95, 0, 95, 1.0)",
-// 			tension: 0.15
-// 		}]
-// 	},
-// 	options: {
-// 		plugins: {
-// 			legend: {
-// 				display: false
-// 			}
-// 		}
-// 	}
-// });
+ new Chart(amzn, {
+ 	type: 'line',
+ 	data: {
+ 		labels: xVals,
+ 		datasets: [{
+ 			label: 'Price',
+ 			data: yVals,
+ 			backgroundColor: 'rgba(115, 0, 115, 1.0)',
+ 			borderColor: "rgba(95, 0, 95, 1.0)",
+ 			tension: 0.15
+ 		}]
+ 	},
+ 	options: {
+ 		plugins: {
+ 			legend: {
+ 				display: false
+ 			}
+ 		}
+ 	}
+});
 
 
-// const goog = document.getElementById('goog');
+ const goog = document.getElementById('goog');
 
-// new Chart(goog, {
-// 	type: 'line',
-// 	data: {
-// 		labels: xVals,
-// 		datasets: [{
-// 			label: 'Price',
-// 			data: yVals,
-// 			backgroundColor: 'rgba(115, 0, 115, 1.0)',
-// 			borderColor: "rgba(95, 0, 95, 1.0)",
-// 			tension: 0.15
-// 		}]
-// 	},
-// 	options: {
-// 		plugins: {
-// 			legend: {
-// 				display: false
-// 			}
-// 		}
-// 	}
-// });
+ new Chart(goog, {
+ 	type: 'line',
+ 	data: {
+ 		labels: xVals,
+ 		datasets: [{
+ 			label: 'Price',
+ 			data: yVals,
+ 			backgroundColor: 'rgba(115, 0, 115, 1.0)',
+ 			borderColor: "rgba(95, 0, 95, 1.0)",
+ 			tension: 0.15
+ 		}]
+ 	},
+ 	options: {
+ 		plugins: {
+ 			legend: {
+ 				display: false
+ 			}
+ 		}
+ 	}
+});
 
-// const voo = document.getElementById('voo');
+ const voo = document.getElementById('voo');
 
-// new Chart(voo, {
-//   type: 'line',
-//   data: {
-//     labels: xVals,
-//     datasets: [{
-//       label: 'Price',
-//       data: yVals,
-//       backgroundColor: 'rgba(115, 0, 115, 1.0)',
-//       borderColor: "rgba(95, 0, 95, 1.0)",
-//       tension: 0.15
-//     }]
-//   },
-//   options: {
-//     plugins:{
-//       legend:{
-//         display: false
-//       }
-//     }
-//   }
-// });
+ new Chart(voo, {
+   type: 'line',
+   data: {
+     labels: xVals,
+     datasets: [{
+       label: 'Price',
+       data: yVals,
+       backgroundColor: 'rgba(115, 0, 115, 1.0)',
+       borderColor: "rgba(95, 0, 95, 1.0)",
+       tension: 0.15
+     }]
+   },
+   options: {
+     plugins:{
+       legend:{
+         display: false
+       }
+     }
+   }
+});
 
 document.addEventListener('DOMContentLoaded', dataGraphing)
->>>>>>> 7a964471ed212a3441d797877b71ff92f5c9601c
